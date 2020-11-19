e93e204dedbb0fd54c7b54a26e3ef4f5add5fd09
Oliver Maurhart <oliver.maurhart@headcode.space>
2020-11-19 17:13:30 +0100

    Initial commit

---
+++ include/headcode/benchmark/version.hpp
@@ -0,0 +1,53 @@
+/*
+ * This file is part of the headcode.space benchmark.
+ *
+ * The 'LICENSE.txt' file in the project root holds the software license.
+ * Copyright (C) 2020 headcode.space
+ * https://www.headcode.space, <info@headcode.space>
+ */
+
+#ifndef HEADCODE_SPACE_BENCHMARK_VERSION_HPP
+#define HEADCODE_SPACE_BENCHMARK_VERSION_HPP
+
+#include <cstdint>
+#include <string>
+
+
+#ifndef MAKE_VERSION
+#define MAKE_VERSION(x, y, z) ((x) << 24 | (y) << 16 | (z))
+#endif
+
+
+/**
+ * @brief   Version check for 0.0.1
+ * @return  A value, representing the version.
+ */
+inline std::uint32_t GetHCSBenchmarkVersion_0_0_1() {
+    return MAKE_VERSION(0, 0, 1);
+}
+
+/**
+ * @brief   The headcode benchmark namespace
+ */
+namespace headcode::benchmark {
+
+/**
+ * @brief   Returns the current version of the headcode-benchmark.
+ * @return  A value, representing the current version.
+ */
+inline std::uint32_t GetCurrentVersion() {
+    return GetHCSBenchmarkVersion_0_0_1();
+}
+
+/**
+ * @brief   Returns a version as string
+ * @return  a string holding the current version.
+ */
+inline std::string GetVersionString() {
+    return "0.0.1";
+}
+
+}
+
+
+#endif
---
+++ test/unit/test_benchmark.cpp
@@ -0,0 +1,18 @@
+/*
+ * This file is part of the headcode.space mem.
+ *
+ * The 'LICENSE.txt' file in the project root holds the software license.
+ * Copyright (C) 2020 headcode.space
+ * https://www.headcode.space, <info@headcode.space>
+ */
+
+#include <gtest/gtest.h>
+
+#include <headcode/benchmark/benchmark.hpp>
+
+using namespace headcode::benchmark;
+
+
+TEST(Benchmark, simple) {
+    ASSERT_TRUE(true);
+}
