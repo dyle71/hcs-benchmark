3b4dbb9859c65e68c40b6d38712c6dc672d05a3a
Oliver Maurhart <oliver.maurhart@headcode.space>
2021-03-25 21:28:50 +0100

    Adds control with cmake to tool section

--- tools/conan/conanfile.py
+++ tools/conan/conanfile.py
@@ -1,11 +1,21 @@
+# ------------------------------------------------------------
+# This file is part of benchmark of headcode.space
+#
+# The 'LICENSE.txt' file in the project root holds the software license.
+# Copyright (C) 2020-2021 headcode.space e.U.
+# Oliver Maurhart <info@headcode.space>, https://www.headcode.space
+# ------------------------------------------------------------
+
 import os
+import os.path
 
 from conans import ConanFile, tools
 
 
 class HcsBenchmarkConan(ConanFile):
+    """Conan package manager file. See https://conan.io"""
     name = "hcs-benchmark"
-    version = "1.0.2"
+    version = "@VERSION@"
     license = "MIT"
     author = "Oliver Maurhart (oliver.maurhart@headcode.space)"
     url = "https://gitlab.com/headcode.space/benchmark"
@@ -16,9 +26,12 @@ class HcsBenchmarkConan(ConanFile):
     # No settings/options are necessary, this is header only
 
     def source(self):
-        self.run("git clone https://gitlab.com/headcode.space/benchmark.git")
+        local_source = "@CMAKE_SOURCE_DIR@"
+        self.run("git clone " + (local_source or "https://gitlab.com/headcode.space/benchmark.git"))
 
     def package(self):
+        with open('foo', 'w') as f:
+            f.write('bar')
         self.copy("*.hpp", src="benchmark/include", dst="include")
 
     def package_id(self):
