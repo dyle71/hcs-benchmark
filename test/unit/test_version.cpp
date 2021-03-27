54b4ecd356f9107856ad68eb5f232de6a04652a6
Oliver Maurhart <oliver.maurhart@headcode.space>
2021-03-27 16:12:23 +0100

    Switches to version v1.1.2

--- test/unit/test_version.cpp
+++ test/unit/test_version.cpp
@@ -30,3 +30,10 @@ TEST(Version, V1_1_1) {
     EXPECT_EQ(static_cast<unsigned int>((1) << 24 | (1) << 16 | (1)), version);
     EXPECT_EQ(GetHCSBenchmarkVersion_1_1_1(), version);
 }
+
+
+TEST(Version, V1_1_2) {
+    std::uint32_t version = MAKE_VERSION(1, 1, 2);
+    EXPECT_EQ(static_cast<unsigned int>((1) << 24 | (1) << 16 | (2)), version);
+    EXPECT_EQ(GetHCSBenchmarkVersion_1_1_2(), version);
+}
